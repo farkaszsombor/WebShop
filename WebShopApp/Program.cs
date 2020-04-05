fa477dcd6ec3d5daa337a170285e32845b41dcfc
Rapa Erik <rapa.csek@gmail.com>
2020-04-05 20:24:20 +0300

    -Delete Web application and create web API

--- WebShopApp/Program.cs
+++ WebShopApp/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WebShop
+namespace WebShopApp
 {
     public class Program
     {
--- WebShopApp/Startup.cs
+++ WebShopApp/Startup.cs
@@ -5,21 +5,20 @@ using System.Threading.Tasks;
 using DataAccessLayer.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace WebShop
+namespace WebShopApp
 {
     public class Startup
     {
-
-        protected static NLog.Logger Logger { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
-            Logger = NLog.LogManager.GetCurrentClassLogger();
         }
 
         public IConfiguration Configuration { get; }
@@ -27,8 +26,7 @@ namespace WebShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-
+            services.AddControllers();
             services.AddDbContext<DataContext>();
         }
 
@@ -39,14 +37,8 @@ namespace WebShop
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -54,9 +46,7 @@ namespace WebShop
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
