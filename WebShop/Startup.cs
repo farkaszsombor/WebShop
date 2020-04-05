ac8e02b3944719c2df659c2943d984188f5a2596
Rapa Erik <rapa.csek@gmail.com>
2020-04-05 18:18:44 +0300

    -Dependency Injection for EF to local MsSql -Add some NuGet packages -Config the NLog

--- WebShop/Startup.cs
+++ WebShop/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLayer.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,9 +14,13 @@ namespace WebShop
 {
     public class Startup
     {
+
+        protected static NLog.Logger Logger { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            Logger = NLog.LogManager.GetCurrentClassLogger();
         }
 
         public IConfiguration Configuration { get; }
@@ -24,6 +29,8 @@ namespace WebShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<DataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
